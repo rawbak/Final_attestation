ee5d92baa6e1d24d3331ad7faadcfebeba56351a
Aleksej <124289255+rawbak@users.noreply.github.com>
2024-12-15 02:56:37 +0300

    update

---
+++ nusery/src/model/Camel.java
@@ -0,0 +1,15 @@
+package nursery.src.model;
+
+import java.util.List;
+
+public class Camel extends PackAnimal {
+    public Camel(String name, List<String> commands) {
+        super(name, commands);
+    }
+
+    @Override
+    public String toString() {
+        String commandsStr = Mapper.mapListToStr(this.getCommands());
+        return "Pack animal. Camel. Name: " + this.getName() + ". Commands: " + commandsStr;
+    }
+}
