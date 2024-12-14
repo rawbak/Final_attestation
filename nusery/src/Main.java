f5d3b24c66a1a76d5a19a75d3f56388cde5c2fc4
Aleksej <124289255+rawbak@users.noreply.github.com>
2024-12-15 02:46:37 +0300

    update

--- nusery/src/Main.java
+++ nusery/src/Main.java
@@ -1,1 +1,15 @@
+package nursery.src;
+import nursery.src.controller.Controller;
+import nursery.src.model.FileOperation;
+import nursery.src.model.Repository;
+import nursery.src.view.View;
 
+public class Main {
+    public static void main(String[] args) {
+        FileOperation fileOperation = new FileOperation("animals.csv");
+        Repository repository = new Repository(fileOperation);
+        Controller controller = new Controller(repository);
+        View view = new View(controller);
+        view.run();
+    }
+}
