7d230b11362fea74d63fc84c74c32eb5f2c254ee
Aleksej <124289255+rawbak@users.noreply.github.com>
2024-12-15 03:05:58 +0300

    update

---
+++ nusery/src/model/PackAnimal.java
@@ -0,0 +1,9 @@
+package nursery.src.model;
+
+import java.util.List;
+
+public abstract class PackAnimal extends Animal {
+    public PackAnimal(String name, List<String> commands) {
+        super(name, commands);
+    }
+}
